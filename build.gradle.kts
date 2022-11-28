e46929cd6d3f4f285dacabcbbed2e2ff6694133e
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-11-28 11:12:06 +0200

    [Oztechan/Assistep#6] Add Kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,21 @@ plugins {
     id("com.android.library").version("8.0.0-alpha08").apply(false)
     kotlin("android").version("1.7.10").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
+    id("org.jetbrains.kotlinx.kover").version("0.6.1")
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlinx:kover:0.6.1")
+    }
+}
+
+allprojects {
+    apply(plugin = "kover")
+}
+
+koverMerged {
+    enable()
 }
 
 tasks.register("clean", Delete::class) {
